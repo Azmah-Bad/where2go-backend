c987ba91d051f56b5bc9cd51735d1c843111fef0
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-08-22 19:37:45 +0200

    beginning setup of authtoken

--- where2go/settings.py
+++ where2go/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'countries',
     'where2go',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
